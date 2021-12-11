5f781d28d5f3cd31cc123def73bea437f3d9de92
rupenman <rupenman>
2021-12-11 15:20:19 -0500

    Changed the approach

--- src/main/java/com/jenkins/test/rest/HelloWorldController.java
+++ src/main/java/com/jenkins/test/rest/HelloWorldController.java
@@ -1,13 +1,14 @@
 package com.jenkins.test.rest;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-  @GetMapping("/hello")
+  @GetMapping(value= "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
   public String hello() {
-    return "Hello World";
+    return "{msg: Hello World}";
   }
 }
