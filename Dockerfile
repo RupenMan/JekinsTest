aa288851a65f5d57036545568ea5bc6b08545d75
rupenman <rupenman>
2021-12-18 01:07:51 -0500

    Docker Test

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,2 @@
-FROM openjdk:12-alpine
 
-COPY $PWD/target/test-0.0.1-SNAPSHOT.jar /home/rupendra/app.jar
 
--- scripts/deploy-service.sh
+++ scripts/deploy-service.sh
@@ -1,5 +1,5 @@
-cd $PWD/target
-ls
-mvn --version
+cp $PWD/target/test-0.0.1-SNAPSHOT.jar /home/rupendra/app.jar
+cd /home/rupendra
+docker build -t test-app .
 systemctl status docker
 
